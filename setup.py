5a2e84a0b9f346c2be8229c96fa6bfa5be2daee1
zhangchunyang <zhangchunyang_pri@126.com>
2022-10-13 09:40:25 +0800

    1.0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 kwargs = {}
 install_requires = []
-version = '1.0.1.6'
+version = '1.0.1.7'
 if sys.version_info < (3, 0):
     with open('README.md') as f:
         kwargs['long_description'] = f.read()
